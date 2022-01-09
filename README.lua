4132690c121e43ffdaac21711ef9e21a8421cb8d
visibait <33099726+visibait@users.noreply.github.com>
2022-01-09 22:23:55 +0100

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,61 @@
+# FiveEye Anticheat
+FiveEye Anticheat was made by VisiBait and Cristian (Apolo). We aim to provide a high quality of service at a cheap cost, our anticheat's current features can be found below.
+
+  
+
+## Anticheat Features 
+```
+-- DETECTIONS --
+⏩ EMD, FMD & OMD (Detects Most Of The Latest Menu's)
+⏩ Anti Executers (Detects Eulen + rE + Desudo)
+⏩ Anti Triggers
+⏩ Anti Explosions
+⏩ Anti Ped Spawn
+⏩ Anti Vehicle Spawn
+⏩ Anti Prop Spawn
+⏩ Anti Particles
+⏩ Anti Trigger-Blocker
+
+-- OTHER DETECTIONS --
+⏩ Anti Aimbot
+⏩ Anti CheatEngine
+⏩ Anti Spectate
+⏩ Anti Radar
+⏩ Anti GodMode
+⏩ Anti SuperJump
+⏩ Anti Weapon Damage Modifiers
+⏩ Anti Thermal Vision
+⏩ Anti Night Vision
+⏩ Anti Resource Injections
+⏩ Anti Resource Start/Stops
+⏩ Anti Vehicle Modifiers
+⏩ Anti Armor Cheats
+⏩ Anti VPN
+⏩ Anti Taze Players
+⏩ Premades
+⏩ NUI Devtools
+
+-- Other Features --
+⏩ Cloud Based Panel For Config + Management
+⏩ Active Staff Team | English + Spanish
+⏩ Unique Anticheat With Optimised Code
+⏩ Unique Ingame Admin System
+```
+
+
+### Important Links
+- [Website](https://fiveeyeac.com)
+- [Discord](https://discord.gg/65JQJZ5v3Q)
+
+
+# FAQ
+
+### What Are Your Prices?
+Weekly: $20
+- Monthly: $45
+- Quarterly: $90
+- Yearly: $200
+
+### What Coding Language Is Your AC?
+
+We coded our anticheat in mainly JS but there is some lua 5.3
